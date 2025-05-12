e4c706760c22192d883f73d085ca03c35d4e10db
Aniruddah Paul <146052552+Anpaul09@users.noreply.github.com>
2025-05-12 16:06:39 -0400

    Update settings.py

--- myProject/settings.py
+++ myProject/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-mwd82b%3ed4%p(c(2-#y8x1z&@ax40nw$@o0v#yx$mk%k)v-i7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['findone-g7lz.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
